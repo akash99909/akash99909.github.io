905659c7196666953dcee94223f9be34de19fe3f
Akash Maurya <47055948+akash99909@users.noreply.github.com>
2020-12-09 02:28:06 +0530

    Update settings.py

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -25,9 +25,9 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 SECRET_KEY = 'hpbv()ep00boce&o0w7z1h)st148(*m@6@-rk$nn)(n9ojj4c0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
